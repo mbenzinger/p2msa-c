2ddc376d82c656f71528ae6b20109c3913386fc9
Mason Benzinger <mason.benzinger@gmail.com>
2022-10-08 14:10:20 -0500

    added mongo configuration, routes, inventory controller route, 404 page

--- app.js
+++ app.js
@@ -9,6 +9,34 @@ app.get("/api", async (req, res) => {
 res.send({ data: "received" });
 });
 const PORT = process.env.PORT || 5000;
+const mongoose = require('mongoose')
+
+
+require('dotenv').config()
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true},
+  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+  )
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('Welcome to the Shoe box')
+  })
+  
+  // INVENTORY
+  const inventoryController = require('./controllers/inventory_controllers')
+  app.use('/inventory', inventoryController)
+  
+  
+  // 404 Page
+  app.get('*', (req, res) => {
+    res.send('404')
+  })
+  
+
+
+//LISTEN
 app.listen(PORT, () => {
 console.log(`Listening on port ${PORT}`);
 });
