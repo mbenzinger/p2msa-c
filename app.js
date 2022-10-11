32c63aa69109dcbecae7e9dbcc3c8a77e9083575
Mason Benzinger <mason.benzinger@gmail.com>
2022-10-10 20:39:45 -0500

    added optional express paths

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 const app = express();
-app.use(express.static(path.resolve(__dirname, "./client/public")));
+// app.use(express.static(path.resolve(__dirname, "./client/public")));
+app.use(express.static(path.resolve(__dirname, ".")));
 app.use(cors());
 app.get("/api", async (req, res) => {
 res.send({ data: "received" });
